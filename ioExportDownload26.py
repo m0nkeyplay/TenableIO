c47c735291ceaffb8336cc4f95e211704817db25
~es <46006871+m0nkeyplay@users.noreply.github.com>
2019-03-20 15:53:48 -0500

    Add files via upload

---
+++ ioExportDownload26.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python -tt
+#
+#	  author:		https://github.com/m0nkeyplay
+#   license:  use this if it works for you 
+#
+# 	purpose: 	Download .nessus or .csv reports for import into SC/reporting tool
+#     			
+#
+#	  structure: 	Open get_files
+#				        Write the files to disk (put_files)
+#               Run after ioExportQueue.py
+
+#   usage:      run after exports have been queued up for download
+#
+#   notes:      fill in the following variables as needed per environment
+#               log_file        <-- Write down what we did
+#               get_files       <-- What files to pick up
+#               put_files       <-- Where to put them
+#               ak              <-- Access Key
+#               sk              <-- Secret Key 
+#               proxies         <-- If you use a proxy, set it here.
+
+import requests
+import json
+import os
+import time
+import datetime
+import sys
+import shutil
+import subprocess
+
+# 	Needed here to get past warning messages when using python 2.6
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+from requests.packages.urllib3.exceptions import SNIMissingWarning
+requests.packages.urllib3.disable_warnings(SNIMissingWarning)
+
+# 	Need an app and secret key for the API to work
+def get_key(keyfile):
+  with open(keyfile,'r') as kf:
+    line = kf.readline().strip()
+    kf.close()
+    return line
+
+def file_date(udate):
+  cdate = datetime.datetime.fromtimestamp(udate)
+  return cdate.strftime('%Y%m%d%H%M%S%f')
+
+  
+cwd = os.getcwd()
+# Feel free to just put the keys in here
+ak = get_key('../.ssh/ak')
+sk = get_key('../.ssh/sk')
+h_key_data = 'accessKey='+ak+'; secretKey='+sk
+log_time = datetime.datetime.now()
+log_file = open(cwd+'', 'a')
+get_files = open(cwd+'', 'r')
+put_files = cwd+''
+check_url = 'https://cloud.tenable.com/scans'
+
+proxies = {}
+proxies['https']= ''
+
+headers = {}
+headers['content-type']= 'application/json'
+headers['x-apikeys']= h_key_data
+
+# 	Status Check
+def status_check(scan,file):
+  url = 'https://cloud.tenable.com/scans/'+scan+'/export/'+file+'/status'
+  r = requests.get(url, proxies=proxies, headers=headers)
+  data = r.json()
+  print data["status"]
+  if data["status"] == 'loading':
+    print 'The file is still being built  Please try again later'
+    return 'loading'
+  else:
+    return 'gtg'
+
+#	Download the files
+def download_report(url,report,con):
+  local_filename = put_files+report+'.'+con
+  r = requests.get(url, proxies=proxies, headers=headers, stream=True)
+  with open(local_filename, 'wb') as f:
+    shutil.copyfileobj(r.raw, f)
+    print >> log_file, str(log_time)+','+local_filename+',created'
+
+def main():
+#	prepare and download the files
+  for line in get_files:
+    line = line.strip()
+    params = line.split(",")
+    r_name = params[0].replace(' ','-')
+    r_name = r_name.lower()
+    scan = params[1]
+    file = params[2]
+    ftype = params[3]
+    download = 'https://cloud.tenable.com/scans/'+scan+'/export/'+file+'/download'
+    if status_check(scan,file) == 'gtg':
+      download_report(download,r_name,ftype)
+
+  get_files.close()
+  log_file.close()
+  
+if __name__ == '__main__':
+  main()
---
+++ scanHistory26.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python -tt
+
+# 	author:  	https://github.com/m0nkeyplay
+#
+# 	purpose: 	Get scan status, start and end time from Tenable.IO
+#               Write data to output and a file
+
+import requests
+import json
+import os
+import time
+import datetime
+import sys
+from datetime import timedelta
+
+args = sys.argv[1:]
+
+if len(args) < 4:
+  print 'usage ./scanHistory.py --scan ScanNametoSearch (encompass spaces in search in quotes) --days NumberOfDaysBackToGo'
+  sys.exit(1)
+
+sscan = ''
+if args[0] == '--scan':
+  sscan = args[1]
+  sdays = '30'
+
+if args[2] != '--days':
+  print 'No --days set.  We will use 30.'  
+  print 'usage ./scanHistory.py --scan ScanNametoSearch (encompass spaces in search in quotes) --days NumberOfDaysBackToGo'
+
+# 	Needed here to get past warning messages when using python 2.6
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+from requests.packages.urllib3.exceptions import SNIMissingWarning
+requests.packages.urllib3.disable_warnings(SNIMissingWarning)
+
+# 	Need an app and secret key for the API to work
+def get_key(keyfile):
+  with open(keyfile,'r') as kf:
+    line = kf.readline().strip()
+    kf.close()
+    return line
+
+def file_date(udate):
+  cdate = datetime.datetime.fromtimestamp(udate)
+  return cdate.strftime('%Y-%m-%d %H:%M:%S')
+  
+def file_date_name(udate):
+  cdate = datetime.datetime.fromtimestamp(udate)
+  return cdate.strftime('%Y%m%d%H%M%S')
+
+cwd = os.getcwd()
+log_name = sscan+'-'+file_date_name(time.time())+'.csv'
+log_file = open(cwd+'/'+log_name, 'w')
+ak = get_key('../.ssh/ak') # use the get key_function or complete it here
+sk = get_key('../.ssh/sk') # use the get key_function or complete it here
+h_key_data = 'accessKey='+ak+'; secretKey='+sk
+check_url = 'https://cloud.tenable.com/scans'
+
+proxies = {}
+proxies['https']= ''
+
+headers = {}
+headers['content-type']= 'application/json'
+headers['x-apikeys']= h_key_data
+
+# See if the scan exists
+def check_scan(url,scan):
+  rv = ''
+  r = requests.get(url, proxies=proxies, headers=headers)
+  data = r.json()
+  for d in data["scans"]:
+    if scan.lower() in d["name"].lower():
+      print 'We found it.  We will move along now.'
+      print 'Scan Name, Completion Status, Start Time, End Time'
+      print >> log_file, 'Scan Name, Completion Status, Start Time, End Time'
+      rv = 'gtg'
+      return rv
+      break
+
+#	Get history and show scans in that period
+def scan_history(url,s_name):
+  r = requests.get(url, proxies=proxies, headers=headers)
+  data = r.json()
+  for i, d in enumerate(data["history"]):
+    s_start = file_date(d["creation_date"])
+    s_end = file_date(d["last_modification_date"])
+    s_status = d["status"]
+    if datetime.datetime.fromtimestamp(d["creation_date"]) >= datetime.datetime.fromtimestamp(time.time()) - timedelta(days=int(sdays)):
+      print s_name+','+s_status+','+s_start+','+s_end
+      print >> log_file,s_name+','+s_status+','+s_start+','+s_end
+      
+
+#	Get the Scan IDs of the scan
+def parse_json(url):
+  r = requests.get(url, proxies=proxies, headers=headers)
+  data = r.json()
+  for d in data["scans"]:
+    if sscan.lower() in d["name"].lower():
+      s_name = d["name"].strip()
+      s_id = str(d["id"])
+      s_url = 'https://cloud.tenable.com/scans/'+s_id
+      scan_history(s_url,s_name)
+
+def main():
+  print 'Searching for '+sscan+' within the past '+sdays+'.  Let\'s see what we find.'
+  if check_scan(check_url,sscan) == 'gtg':
+    parse_json(check_url)
+    log_file.close()
+    print ''
+    print 'Complete.  Data written to '+cwd+'/'+log_name
+  else:
+    log_file.close()
+    print 'Complete.  No scans with that name found.'
+    print 'May want do delete '+cwd+'/'+log_name+ ' since it\'s empty.'
+  
+if __name__ == '__main__':
+  main()
