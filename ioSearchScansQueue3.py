9da3411118bddb20a7b5335b521e75641a262864
~es <46006871+m0nkeyplay@users.noreply.github.com>
2019-04-09 21:09:57 -0500

    added compliance check - check ReadMe

--- ioSearchScansQueue3.py
+++ ioSearchScansQueue3.py
@@ -13,7 +13,7 @@
 #
 #   switchs:    -scan       Search this specific scan
 #               -o          Output Type options:  nessus, csv
-#               -q          Query Type options:  pluginid, pluginname, hostname, riskfactor
+#               -q          Query Type options:  pluginid, pluginname, hostname, riskfactor, compliancecheck
 #               -d or -f    -d for one...   example: -q pluginid -d 19506
 #                           -f for file...  -f /path/to/file              
 #
@@ -62,7 +62,7 @@ usage = '\n usage% python3 ioSearchScansQueue3.py'
 usage += ' -scan ScanNametoSearch -o nessus|csv -q filterQuery -d datapoint | -f /path/to/file\n'
 usage += '\nswitchs:\n-scan       Search this specific scan *see below '
 usage += '\n-o          Output Type options:  nessus, csv'
-usage += '\n-q          Query Type options:  pluginid, pluginname, hostname, riskfactor'
+usage += '\n-q          Query Type options:  pluginid, pluginname, hostname, riskfactor, compliancecheck'
 usage += '\n-d or -f    '
 usage += '\n            -d for one...   example: -q pluginid -d 19506'
 usage += '\n            -f for file...  example: -q pluginid -f /path/to/file/with/a/list/of/pluginids'
@@ -79,6 +79,7 @@ apiFilter['pluginid'] = ('plugin.id','eq')
 apiFilter['pluginname'] = ('plugin.name','match')
 apiFilter['hostname'] = ('host.hostname','match')
 apiFilter['riskfactor'] = ('plugin.attributes.risk_factor','eq')
+apiFilter['compliancecheck'] = ('compliance_description','match')
 
 #   We are creating the search string with the function if we are getting the data from a file
 #   Takes the query, matches it to the dictionary item and away we go
